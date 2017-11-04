398be6a281645289b5a745508359017022b0c60f
Grayson Pike <graysonpike@gmail.com>
2017-11-03 22:02:46 -0500

    Refactoring/commenting

--- names.py
+++ names.py
@@ -6,41 +6,25 @@ import sys
 
 
 # keywords = [
-#     "achilles",
-#     "helen",
-#     "priam",
-#     "paris",
-#     "agamemnon",
-#     "hector",
-#     "atrides",
-#     "patroclus",
-#     "odysseus",
-#     "diomedes",
-#     "tydides",a
-#     "ajax",
+#     "zeus",
+#     "juno",
+#     "jupiter",
+#     "jove",
 # ]
 
+# keywords = [
+#     "paris",
+#     "alexander",
+#     "alexandrus",
+#     "alexandros",
+# ]
 
-keywords = [
-    "zeus",
-    "juno",
-    "jupiter",
-    "jove",
-]
-
-keywords = [
-    "paris",
-    "alexander",
-    "alexandrus",
-    "alexandros",
-]
-
-keywords = [
-    "diomedes",
-    "tydides",
-    "diomede",
-    "diomed",
-]
+# keywords = [
+#     "diomedes",
+#     "tydides",
+#     "diomede",
+#     "diomed",
+# ]
 
 keywords = [
     "agamemnon",
@@ -61,9 +45,9 @@ def getOccurences(text, keywords):
 
 def main():
 
+    # Read files for all texts
     print("Parsing plaintexts: ", end='')
     sys.stdout.flush()
-    # Read files for all texts
     texts = {}
     for text in common.TEXTS:
         texts[text] = common.readFile("translations/cleaned/" + text + ".txt")
@@ -71,12 +55,15 @@ def main():
 
     results = {}
 
+    # Check # of occurences for each keyword in each text
     print("Evaluating texts:   ", end="")
     sys.stdout.flush()
     for text in texts:
         results[text] = getOccurences(texts[text], keywords)
     print("[OK]")
 
+    # Add resulting data to a 2d array so that it can be sorted
+    # by publication date
     table_data = []
     for text in results:
         line = "{0:30}".format(text)
@@ -85,6 +72,7 @@ def main():
         table_data.append((line, common.PUB_DATES[text]))
     table_data = sorted(table_data, key=lambda x: x[1])
 
+    # Print data in formatted table
     header = ("{0:30}".format("Translator"))
     for keyword in keywords:
         header += ("{0:15}".format(keyword))
